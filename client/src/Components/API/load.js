0dc5e84f4458c402fa3ef560ea928a2ba3381093
Oleg <bulgakov.exceedteam@gmail.com>
2019-07-11 10:54:21 +0300

    set get/alll

--- client/src/Components/API/load.js
+++ client/src/Components/API/load.js
@@ -1,16 +1,15 @@
-// import axios from 'axios';
-//
-// export default () => {
-//     return new Promise((resolve, reject) => {
-//         axios.get('/')
-//             .then(data => {
-//                 console.log('qweqweqweqwe', data)
-//                 resolve(data);
-//             }, (err) => {
-//                 console.warn(err);
-//                 reject(err);
-//             })
-//     })
-// };
-//
-//
+import axios from 'axios';
+
+export default () => {
+    return new Promise((resolve, reject) => {
+        axios.get('/all')
+            .then(data => {
+                resolve(data);
+            }, (err) => {
+                console.warn(err);
+                reject(err);
+            })
+    })
+};
+
+
--- client/src/Components/TodoList/TodoList.js
+++ client/src/Components/TodoList/TodoList.js
@@ -24,10 +24,9 @@ class TodoList extends Component {
     }
 
     loadData() {
-        axios.get('/all')
-        .then(res => {
-            console.log('get', res);
-                const itemsToInsert = res.data.todoitems.map((item) => {
+        load().then(data => {
+            console.log(data);
+                const itemsToInsert = data.data.todoitems.map((item) => {
                     return {
                         key: item._id,
                         text: item.text,
@@ -47,7 +46,6 @@ class TodoList extends Component {
             };
             axios.post(`/`, {newItem})
                 .then((res) => {
-                    console.log('postttt', res)
                     const newItem = {
                         key: res.data.item._id,
                         text: res.data.item.text,
