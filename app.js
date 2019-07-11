285e429c0eb9dafd5a4b25f728e516ebfb002345
Oleg <bulgakov.exceedteam@gmail.com>
2019-07-11 13:01:32 +0300

    Change app.js

--- app.js
+++ app.js
@@ -18,22 +18,31 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 const path = require('path');
 // Serve static files from the React frontend app
-app.use(express.static(path.join('client/build')));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join('client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Anything that doesn't match the above, send back index.html
-app.get('*', (req, res) => {
-  res.sendFile(path.join('/client/build/index.html'))
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join('/client/build/index.html'))
+// });
 
 require('./models/task.model');
 
 require('./routes/task.route')(app);
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-});
 
+if (process.env.NODE_ENV === 'production') {
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
