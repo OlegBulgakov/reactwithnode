a40cd48db581c1f8b6c0dc7ead59ab94b77286c4
Oleg <bulgakov.exceedteam@gmail.com>
2019-07-11 11:44:59 +0300

    fix routes

--- client/src/Components/API/load.js
+++ client/src/Components/API/load.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default () => {
     return new Promise((resolve, reject) => {
-        axios.get('/api/todos')
+        axios.get('/api/todos/all')
             .then(data => {
                 resolve(data);
             }, (err) => {
